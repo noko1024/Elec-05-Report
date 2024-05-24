ed8a94a9508748c31bcdeac6dbe5c87e76e9ba32
Yuhei Kobayashi <44843376+noko1024@users.noreply.github.com>
2024-05-24 10:59:52 +0900

    Create 06.py

---
+++ 06.py
@@ -0,0 +1,49 @@
+import csv 
+import sys
+import datetime
+
+
+source = {
+    "Mon":[],
+    "Tue":[],
+    "Wed":[],
+    "Thu":[],
+    "Fri":[],
+    "Sat":[],
+    "Sun":[]
+    }
+
+with open("./tepco2023_4.csv") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        tdatatime = datetime.datetime.strptime(row[0]+","+row[1], '%Y/%m/%d,%H:%M')
+        week = tdatatime.strftime("%a")
+        day = datetime.date(tdatatime.year,tdatatime.month,tdatatime.day)
+
+        data = source[week]
+        data.append([day,row[2]])
+        source[week] = data
+
+
+m1 = source.get("Mon")[0:24]
+m2 = source.get("Mon")[25:49]
+m3 = source.get("Mon")[50:74]
+m4 = source.get("Mon")[75:99]
+
+source.get("Moon")[100]
+
+#pit.plot(x,result.T[0],marker="^",label="1")
+#pit.plot(x,result.T[1],marker="v",label="2")
+#pit.plot(x,result.T[2],marker="D",label="3")
+#pit.plot(x,avarage,marker=".",label="avarage")
+#
+#pit.title("Positive Electrode")
+#pit.ylabel("Spark Voltage (kV)")
+#pit.xlabel("Gap Length (cm)")
+#
+#pit.legend()
+#pit.grid(True)
+#
+#plt.figure() 
+#
+#pit.show()
