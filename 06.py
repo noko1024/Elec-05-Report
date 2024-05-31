0d719db519f86f691ebc1cfa0ef6e3491d41ee89
noko1024 <ynoko1024@gmail.com>
2024-05-31 09:18:23 +0900

    tmp

--- 06.py
+++ 06.py
@@ -1,27 +1,28 @@
-import csv 
+import csv
 import sys
 import datetime
 
 
 source = {
-    "Mon":[],
-    "Tue":[],
-    "Wed":[],
-    "Thu":[],
-    "Fri":[],
-    "Sat":[],
-    "Sun":[]
-    }
+    "Mon": [],
+    "Tue": [],
+    "Wed": [],
+    "Thu": [],
+    "Fri": [],
+    "Sat": [],
+    "Sun": []
+}
 
 with open("./tepco2023_4.csv") as f:
     reader = csv.reader(f)
     for row in reader:
-        tdatatime = datetime.datetime.strptime(row[0]+","+row[1], '%Y/%m/%d,%H:%M')
+        tdatatime = datetime.datetime.strptime(
+            row[0]+","+row[1], '%Y/%m/%d,%H:%M')
         week = tdatatime.strftime("%a")
-        day = datetime.date(tdatatime.year,tdatatime.month,tdatatime.day)
+        day = datetime.date(tdatatime.year, tdatatime.month, tdatatime.day)
 
         data = source[week]
-        data.append([day,row[2]])
+        data.append([day, row[2]])
         source[week] = data
 
 
@@ -30,20 +31,20 @@ m2 = source.get("Mon")[25:49]
 m3 = source.get("Mon")[50:74]
 m4 = source.get("Mon")[75:99]
 
-source.get("Moon")[100]
+print(source.get("Mon")[0])
 
-#pit.plot(x,result.T[0],marker="^",label="1")
-#pit.plot(x,result.T[1],marker="v",label="2")
-#pit.plot(x,result.T[2],marker="D",label="3")
-#pit.plot(x,avarage,marker=".",label="avarage")
+# pit.plot(x,result.T[0],marker="^",label="1")
+# pit.plot(x,result.T[1],marker="v",label="2")
+# pit.plot(x,result.T[2],marker="D",label="3")
+# pit.plot(x,avarage,marker=".",label="avarage")
 #
-#pit.title("Positive Electrode")
-#pit.ylabel("Spark Voltage (kV)")
-#pit.xlabel("Gap Length (cm)")
+# pit.title("Positive Electrode")
+# pit.ylabel("Spark Voltage (kV)")
+# pit.xlabel("Gap Length (cm)")
 #
-#pit.legend()
-#pit.grid(True)
+# pit.legend()
+# pit.grid(True)
 #
-#plt.figure() 
+# plt.figure()
 #
-#pit.show()
+# pit.show()
---
+++ test.py
@@ -0,0 +1,36 @@
+import csv
+import sys
+import datetime
+import matplotlib.pyplot as plt
+
+source = {
+    "Mon": [],
+    "Tue": [],
+    "Wed": [],
+    "Thu": [],
+    "Fri": [],
+    "Sat": [],
+    "Sun": []
+}
+
+with open('./tepco2023_4.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    target_week = ""
+    for row in reader:
+        day = datetime.datetime.strptime(row[0], '%Y/%m/%d')
+        week = day.strftime('%a')
+
+        if week != target_week:
+            target_week = week
+            data = source[week]
+            data.append([])
+            source[week] = data
+
+        data = source[week]
+        data[-1].append([day, row[2]])
+        source[week] = data
+
+plt.legend()
+plt.grid()
+plt.show()
+fig = plt.figure()
